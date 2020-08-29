b01484ab80318a6da0e352a8618f71e82f435acb
Humberto Filho <humbertofilho5@gmail.com>
2020-08-29 12:46:36 -0400

    autenticação

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { getToken } from "./auth";
+
+// baseURL API
+const api = axios.create({
+  baseURL: "https://navedex-api.herokuapp.com/v1",
+});
+
+/**
+Here the Axios interceptors were used,
+ as the name suggests it intercepts a request before it actually happens,
+  at that moment it is checked if there is a token in the localStorage,
+   and if it exists, it adds the Authorization Header to the request.
+ */
+api.interceptors.request.use(async (config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;

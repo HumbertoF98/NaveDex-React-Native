ae88ade4053dbc98f3283d2486141f91e4950d44
Humberto Filho <humbertofilho5@gmail.com>
2020-08-30 09:53:03 -0400

    finalizado 3.0

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,12 +4,6 @@ import { Container, LogoImage, Form } from "./styles";
 import { Alert } from "react-native";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
-import { AppLoading } from "expo";
-import {
-  useFonts,
-  Montserrat_400Regular,
-  Montserrat_600SemiBold,
-} from "@expo-google-fonts/montserrat";
 // storage data of cellphone
 import AsyncStorage from "@react-native-community/async-storage";
 //api
@@ -20,14 +14,6 @@ export default function SignIn({ navigation }) {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
-  let [fontsLoaded] = useFonts({
-    Montserrat_400Regular,
-    Montserrat_600SemiBold,
-  });
-
-  // await to load font
-  if (!fontsLoaded) {
-    return <AppLoading />;
   // refference to password
   const passwordRef = useRef();
 
